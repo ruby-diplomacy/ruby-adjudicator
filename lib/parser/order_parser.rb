c0c2a5c0ce92114725309581b6395514646c492a
NamelessOne <unfortunate42@gmail.com>
2013-08-24 14:27:05 +0200

    Added parsing exceptions.

--- lib/parser/order_parser.rb
+++ lib/parser/order_parser.rb
@@ -17,9 +17,8 @@ module Diplomacy
     
     def parse_single_order(orderblob)
       # try to parse it as a move
-      /^[AF](?'unit_area'\w{3})(?'unit_area_coast'\(.+?\))?-(?'dst'\w{3})(?'dst_coast'[^-]+)?$/ =~ orderblob
-      if not unit_area.nil?
-        unit = @gamestate[unit_area.to_sym].unit unless unit_area.nil?
+      if /^[AF](?'unit_area'\w{3})(?'unit_area_coast'\(.+?\))?-(?'dst'\w{3})(?'dst_coast'.+)?$/ =~ orderblob
+        unit = @gamestate[unit_area.to_sym].unit
         move = Move.new(unit, unit_area.to_sym, dst.to_sym)
         move.unit_area_coast = unit_area_coast if not unit_area_coast.nil?
         move.dst_coast = dst_coast if not dst_coast.nil?
@@ -27,15 +26,13 @@ module Diplomacy
       end
       
       # try to parse it as a hold
-      /^[AF](?'unit_area'\w{3})H$/ =~ orderblob
-      if not unit_area.nil?
+      if /^[AF](?'unit_area'\w{3})H$/ =~ orderblob
         unit = @gamestate[unit_area.to_sym].unit unless unit_area.nil?
         return Hold.new(unit, unit_area.to_sym)
       end
       
       # try to parse it as a support
-      /^[AF](?'unit_area'\w{3})(?'unit_area_coast'\(.+?\))?S[AF](?'src'\w{3})(?'src_coast'\(.+?\))?-(?'dst'\w{3})(?'dst_coast'[^-]+)?$/ =~ orderblob
-      if not unit_area.nil?
+      if /^[AF](?'unit_area'\w{3})(?'unit_area_coast'\(.+?\))?S[AF](?'src'\w{3})(?'src_coast'\(.+?\))?-(?'dst'\w{3})(?'dst_coast'.+)?$/ =~ orderblob
         unit = @gamestate[unit_area.to_sym].unit unless unit_area.nil?
         support = Support.new(unit, unit_area.to_sym, src.to_sym, dst.to_sym)
         support.unit_area_coast = unit_area_coast if not unit_area_coast.nil?
@@ -45,8 +42,7 @@ module Diplomacy
       end
       
       # try to parse it as a support hold
-      /^[AF](?'unit_area'\w{3})(?'unit_area_coast'\(.+?\))?S[AF](?'dst'\w{3})(?'dst_coast'\(.+?\))?$/ =~ orderblob
-      if not unit_area.nil?
+      if /^[AF](?'unit_area'\w{3})(?'unit_area_coast'\(.+?\))?S[AF](?'dst'\w{3})(?'dst_coast'\(.+?\))?$/ =~ orderblob
         unit = @gamestate[unit_area.to_sym].unit unless unit_area.nil?
         support = SupportHold.new(unit, unit_area.to_sym, dst.to_sym)
         support.unit_area_coast = unit_area_coast if not unit_area_coast.nil?
@@ -55,11 +51,12 @@ module Diplomacy
       end
       
       # try to parse it as a convoy
-      /^[AF](?'unit_area'\w{3})C[AF](?'src'\w{3})-(?'dst'\w{3})$/ =~ orderblob
-      if not unit_area.nil?
+      if /^[AF](?'unit_area'\w{3})C[AF](?'src'\w{3})-(?'dst'\w{3})$/ =~ orderblob
         unit = @gamestate[unit_area.to_sym].unit unless unit_area.nil?
         return Convoy.new(unit, unit_area.to_sym, src.to_sym, dst.to_sym)
       end
+
+      raise OrderParsingError.new(orderblob), 'order didn\'t match any order type'
     end
 
     def parse_retreats(orderblob)
@@ -74,9 +71,8 @@ module Diplomacy
 
     def parse_single_retreat(orderblob)
       # try to parse it as a move
-      /^[AF](?'unit_area'\w{3})(?'unit_area_coast'\(.+?\))?-(?'dst'\w{3})(?'dst_coast'[^-]+)?$/ =~ orderblob
-      if not unit_area.nil?
-        unit = @gamestate[unit_area.to_sym].unit unless unit_area.nil?
+      if /^[AF](?'unit_area'\w{3})(?'unit_area_coast'\(.+?\))?-(?'dst'\w{3})(?'dst_coast'[^-]+)?$/ =~ orderblob
+        unit = @gamestate[unit_area.to_sym].unit
         retreat = Retreat.new(unit, unit_area.to_sym, dst.to_sym)
         retreat.unit_area_coast = unit_area_coast if not unit_area_coast.nil?
         retreat.dst_coast = dst_coast if not dst_coast.nil?
@@ -95,8 +91,7 @@ module Diplomacy
     end
 
     def parse_single_build(orderblob)
-      /^(?'unit_type'[AF])(?'unit_area'\w{3})(?'build'[BD])$/ =~ orderblob
-      if not unit_area.nil?
+      if /^(?'unit_type'[AF])(?'unit_area'\w{3})(?'build'[BD])$/ =~ orderblob
       	is_build = (build == 'B')
         is_army = (unit_type == 'A')
       	if is_build
@@ -107,6 +102,14 @@ module Diplomacy
 
         return Build.new(unit, unit_area.to_sym, is_build)
       end
+    end
+  end
+
+  class OrderParsingError < StandardError
+    attr_accessor :orderblob
+
+    def initialize(orderblob)
+      @orderblob = orderblob
     end
   end
 end
